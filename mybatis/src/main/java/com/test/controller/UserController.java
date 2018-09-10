acea22663eea9fc869fd773b63565e7455db3bbf
builthuLin <1260958005@qq.com>
2018-09-10 22:50:12 +0800

    【用户新增以及主键返回】

--- mybatis/src/main/java/com/test/controller/UserController.java
+++ mybatis/src/main/java/com/test/controller/UserController.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 import com.test.pojo.User;
 
 public class UserController {
@@ -38,6 +39,60 @@ public class UserController {
 		System.out.println(user);
 		
 		//释放资源
+		sqlSession.close();
+	}
+	
+	/**
+	 * 根据用户名称模糊查询用户列表
+	 * @throws IOException 
+	 */
+	@Test
+	public void findUserByName() throws IOException {
+		//mybatis配置文件
+		String resource = "SqlMapConfig.xml";
+		
+		//得到配置文件流
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		
+		//创建会话工厂,传入mybatis配置文件信息
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		
+		//通过工厂得到sqlsession
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+	    
+		java.util.List<User> list = sqlSession.selectList("test.findUserByName", "小明");
+		System.out.println(list);
+		sqlSession.close();
+	}
+	
+	/**
+	 * 新增用户
+	 * @throws IOException 
+	 */
+	@Test
+	public void insertUser() throws IOException {
+		//mybatis配置文件
+		String resource = "SqlMapConfig.xml";
+		
+		//得到配置文件流
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		
+		//创建会话工厂,传入mybatis配置文件信息
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		
+		//通过工厂得到sqlsession
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		User user = new User();
+		user.setUsername("王小军");
+		user.setPassword("123456");
+		user.setNotes("");
+		//提交user和映射文件中的resultType所指的类型一致
+		int ret = sqlSession.insert("test.insertUser", user);
+		
+		//提交事物
+		sqlSession.commit();
+		System.out.println("===================================="+ret+user.getId());
 		sqlSession.close();
 	}
 }
